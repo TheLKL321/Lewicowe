92f1fe7126f8ffcabe5b04d33795ad9b081fb511
Łukasz Zarębski <lkl.zarebski@gmail.com>
2017-11-08 15:04:02 +0100

    initial commit

---
+++ leftist.ml
@@ -0,0 +1,23 @@
+type a' queue =
+  | Node of a' queue * a' * a' queue
+  | Null
+
+let empty = Null
+
+let add a q =
+
+exception Empty;;
+
+let delete_min =
+  |
+
+  | Null -> Empty
+
+let join q1 q2 =
+
+let is_empty q =
+  if q = Null then
+    true
+  else
+    false
+;;
